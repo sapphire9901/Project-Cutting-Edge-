5c93b5bb0991118f6b3b809c089fb721d8525462
Cecilia <cecili26a@gmail.com>
2025-02-03 10:54:44 +0100

    Skrevet kode for hamburgermeny

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar') /*select sidebar element*/
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar(){
+     const sidebar = document.querySelector('.sidebar') /*select sidebar element*/
+     sidebar.style.display = 'none'
+
+}
