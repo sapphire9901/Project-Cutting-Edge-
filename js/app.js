dff2281a0d5d011113d574b0921ea6973337ecae
sapphire9901 <sagro5021@oslomet.no>
2025-03-13 18:40:20 +0100

    laget ny funksjon i js

--- js/app.js
+++ js/app.js
@@ -12,3 +12,23 @@ function hideSidebar(){
 function handleLogoClick() {
     window.location.href = "index.html"; // Change this to your desired page or action
 }
+
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+
+        }
+    }
+}
